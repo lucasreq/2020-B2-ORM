9acdefb0e2bbfa1ead44a13de513d959bdf23fb2
Requena <lucas.requena@ynov.com>
2019-10-23 11:16:30 +0200

    first

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,6 +7,36 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    let gain1 = 1000
+    let gain2 = 100
+    let perdu = "KO"
+    let score = 0
+
+    
+    let score_dep = input.slice(0,1)
+    let start = input.shift(0)
+    
+
+    console.log(score_dep)
+
+    score = score + score_dep
+
+    
+
+    /*console.log(score_dep)*/
+
+    if (score > 0 && score < 100){
+      return gain1
+    }
+    else if (score > 100 && score < 10000){
+      return gain2
+    }
+    else {
+      return perdu
+    }
+    
+  
+
 
     // AND HERE
   },
